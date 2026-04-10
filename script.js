0d2d04183ba5080b1776589c28e5ef04d77c3420
R.D.K.S. Kumara <134190302+sanjayakasun@users.noreply.github.com>
2026-04-10 13:26:46 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,81 @@
+// AGE CALCULATION
+document.getElementById("birthday").addEventListener("change", function () {
+  let birth = new Date(this.value);
+  let refDate = new Date("2025-06-30");
+
+  let years = refDate.getFullYear() - birth.getFullYear();
+  let months = refDate.getMonth() - birth.getMonth();
+  let days = refDate.getDate() - birth.getDate();
+
+  if (days < 0) {
+    months--;
+    days += 30;
+  }
+
+  if (months < 0) {
+    years--;
+    months += 12;
+  }
+
+  document.getElementById("age").value =
+    `${years} years ${months} months ${days} days`;
+});
+
+
+// SUBMIT FORM
+document.getElementById("form").addEventListener("submit", async function (e) {
+  e.preventDefault();
+
+  let nic = document.getElementById("nic").value;
+  let mobile = document.getElementById("mobile").value;
+
+  // VALIDATION
+  if (!/^([0-9]{9}[vVxX]|[0-9]{12})$/.test(nic)) {
+    document.getElementById("msg").innerHTML = "Invalid NIC";
+    return;
+  }
+
+  if (!/^07[0-9]{8}$/.test(mobile)) {
+    document.getElementById("msg").innerHTML = "Invalid Mobile";
+    return;
+  }
+
+  const form = document.getElementById("form");
+  const formData = new FormData(form);
+
+  // FIX CHECKBOX
+  let selectedDistricts = [];
+  document.querySelectorAll('input[name="selectedDistricts"]:checked')
+    .forEach(el => selectedDistricts.push(el.value));
+
+const data = {};
+
+// manually collect ALL fields correctly
+formData.forEach((value, key) => {
+  data[key] = value;
+});
+
+// DEBUG (VERY IMPORTANT)
+console.log("FINAL DATA:", data);
+
+  document.getElementById("msg").innerHTML = "Submitting...";
+
+  try {
+    const response = await fetch("https://script.google.com/macros/s/AKfycbzgQY11v5hKBNomfOcpcVcrXAgjtJrbMcPOXUeBzl3c-9rV1SGeg49KDgmgtPexjpZC5g/exec", {
+      method: "POST",
+      body: JSON.stringify(data)
+    });
+      console.log(data);
+    const result = await response.json();
+
+    if (result.status === "success") {
+      document.getElementById("msg").innerHTML = "✅ Submitted successfully";
+      form.reset();
+    } else {
+      document.getElementById("msg").innerHTML = "❌ Failed";
+    }
+
+  } catch (error) {
+    document.getElementById("msg").innerHTML = "❌ Error";
+  }
+});
